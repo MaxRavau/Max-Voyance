c7a0da588a65cc08231e8595617cf44b677efba2
MaxRavau <max.ravau@gmail.com>
2017-04-12 14:48:18 +0200

    Suite

--- Max-Voyance/ViewController.swift
+++ Max-Voyance/ViewController.swift
@@ -9,12 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var viewLabelPhrase: UIView!
+    @IBOutlet var viewEffet: UIVisualEffectView!
+    @IBOutlet var labelPhrase: UILabel!
+    
+    var effet: UIVisualEffect!
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        effet = viewEffet.effect
+        viewEffet.effect = nil
+        
+        viewLabelPhrase.layer.cornerRadius = 5
+        viewLabelPhrase.layer.borderColor = UIColor.black.cgColor
+        viewLabelPhrase.layer.borderWidth = 1
+        
+    }
+    
+    func animateIn(){
+        
+        self.view.addSubview(viewLabelPhrase)
+        viewLabelPhrase.center = self.view.center
+        
+        viewLabelPhrase.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        viewLabelPhrase.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            
+            self.viewEffet.effect = self.effet
+            self.viewLabelPhrase.alpha = 1
+            self.viewLabelPhrase.transform = CGAffineTransform.identity
+            
+            
+        
+    }
+    
+}
+    func animateOut(){
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            
+            self.viewLabelPhrase.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.viewLabelPhrase.alpha = 0
+            
+            self.viewEffet.effect = nil
+            
+        }) { (success: Bool) in
+            self.viewLabelPhrase.removeFromSuperview()
+        }
     }
 
+    @IBAction func buttonTapView(_ sender: Any) {
+    
+        animateIn()
+
+    }
+    
+
+    @IBAction func dismissButtonTap(_ sender: Any) {
+        
+        animateOut()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
