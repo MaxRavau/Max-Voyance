c63402871bc89308de8b2551f0f38f26c8740eeb
MaxRavau <max.ravau@gmail.com>
2017-04-12 11:56:09 +0200

    Start Max Voyance

---
+++ Max-Voyance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Max-Voyance
+//
+//  Created by Maxime Ravau on 12/04/2017.
+//  Copyright © 2017 Maxime Ravau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
